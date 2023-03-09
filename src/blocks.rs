2b1296d16489f5e19032a79fa53b41ccefcd0b7d
macha <kazuki.aishima.dev@gmail.com>
2023-03-09 15:01:35 +0900

    first release

---
+++ src/blocks.rs
@@ -0,0 +1,77 @@
+use block_kind::{I, J, L, O, S, T, Z};
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
+
+#[derive(Clone, Copy)]
+pub enum Blockkind {
+    I,
+    O,
+    S,
+    Z,
+    J,
+    L,
+    T,
+}
+
+pub type BlockColor = usize;
+
+pub mod block_kind {
+    pub const NONE: super::BlockColor = 0;
+    pub const WALL: super::BlockColor = 1;
+    pub const GHOST: super::BlockColor = 2;
+    pub const I: super::BlockColor = 3;
+    pub const O: super::BlockColor = 4;
+    pub const S: super::BlockColor = 5;
+    pub const Z: super::BlockColor = 6;
+    pub const J: super::BlockColor = 7;
+    pub const L: super::BlockColor = 8;
+    pub const T: super::BlockColor = 9;
+}
+
+pub const COLOR_TABLE: [&str; 10] = [
+    "\x1b[48;2;000;000;000m  ", // 何もなし
+    "\x1b[48;2;127;127;127m__", // 壁
+    "\x1b[48;2;000;000;000m[]",  // ゴースト
+    "\x1b[48;2;000;000;255m__", // I
+    "\x1b[48;2;000;255;000m__", // O
+    "\x1b[48;2;000;255;255m__", // S
+    "\x1b[48;2;255;000;000m__", // Z
+    "\x1b[48;2;255;000;255m__", // J
+    "\x1b[48;2;255;127;000m__", // L
+    "\x1b[48;2;255;255;000m__", // T
+];
+
+impl Distribution<Blockkind> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Blockkind {
+        match rng.gen_range(0..=6) {
+            0 => Blockkind::I,
+            1 => Blockkind::O,
+            2 => Blockkind::S,
+            3 => Blockkind::Z,
+            4 => Blockkind::J,
+            5 => Blockkind::L,
+            _ => Blockkind::T,
+        }
+    }
+}
+
+pub type BlockShape = [[usize; 4]; 4];
+
+pub const BLOCKS: [BlockShape; 7] = [
+    // I block
+    [[0, 0, 0, 0], [0, 0, 0, 0], [I, I, I, I], [0, 0, 0, 0]],
+    // O block
+    [[0, 0, 0, 0], [0, O, O, 0], [0, O, O, 0], [0, 0, 0, 0]],
+    // S block
+    [[0, 0, 0, 0], [0, S, S, 0], [S, S, 0, 0], [0, 0, 0, 0]],
+    // Z block
+    [[0, 0, 0, 0], [Z, Z, 0, 0], [0, Z, Z, 0], [0, 0, 0, 0]],
+    // J block
+    [[0, 0, 0, 0], [J, 0, 0, 0], [J, J, J, 0], [0, 0, 0, 0]],
+    // L block
+    [[0, 0, 0, 0], [0, 0, L, 0], [L, L, L, 0], [0, 0, 0, 0]],
+    // T block
+    [[0, 0, 0, 0], [0, T, 0, 0], [T, T, T, 0], [0, 0, 0, 0]],
+];
---
+++ src/game.rs
@@ -0,0 +1,258 @@
+use crate::blocks::{block_kind, block_kind::WALL as W, BlockColor, COLOR_TABLE};
+use crate::blocks::{BlockShape, Blockkind, BLOCKS};
+
+pub const FIELD_WIDTH: usize = 11 + 2 + 2;
+pub const FIELD_HEIGHT: usize = 20 + 1 + 1;
+pub type Field = [[BlockColor; FIELD_WIDTH]; FIELD_HEIGHT];
+
+#[derive(Clone, Copy)]
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
+impl Position {
+    pub fn init() -> Position {
+        Position { x: 5, y: 0 }
+    }
+}
+
+pub struct Game {
+    pub field: Field,
+    pub pos: Position,
+    pub block: BlockShape,
+}
+
+impl Game {
+    pub fn new() -> Game {
+        Game {
+            field: [
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, W, W, W, W, W, W, W, W, W, W, W, W, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ],
+            pos: Position::init(),
+            block: BLOCKS[rand::random::<Blockkind>() as usize],
+        }
+    }
+}
+
+#[allow(clippy::needless_range_loop)]
+pub fn draw(Game { field, pos, block }: &Game) {
+    let mut field_buf = *field;
+
+    let ghost_pos = ghost_pos(field, pos, block);
+    for y in 0..4 {
+        for x in 0..4 {
+            if block[y][x] != block_kind::NONE {
+                field_buf[y + ghost_pos.y][x + ghost_pos.x] = block_kind::GHOST;
+            }
+        }
+    }
+
+    for y in 0..4 {
+        for x in 0..4 {
+            if block[y][x] != block_kind::NONE {
+                field_buf[y + pos.y][x + pos.x] = block[y][x];
+            }
+        }
+    }
+
+    println!("\x1b[H");
+
+    for y in 0..FIELD_HEIGHT - 1 {
+        for x in 1..FIELD_WIDTH - 1 {
+            print!("{}", COLOR_TABLE[field_buf[y][x]]);
+        }
+        println!();
+    }
+}
+
+pub fn is_collision(field: &Field, pos: &Position, block: &BlockShape) -> bool {
+    for y in 0..4 {
+        for x in 0..4 {
+            if y + pos.y >= FIELD_HEIGHT || x + pos.x >= FIELD_WIDTH {
+                continue;
+            }
+            if block[y][x] != block_kind::NONE && field[y + pos.y][x + pos.x] != block_kind::NONE {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+pub fn fix_block(Game { field, pos, block }: &mut Game) {
+    for y in 0..4 {
+        for x in 0..4 {
+            if block[y][x] != block_kind::NONE {
+                field[y + pos.y][x + pos.x] = block[y][x];
+            }
+        }
+    }
+}
+
+pub fn erase_line(field: &mut Field) {
+    for y in 1..FIELD_HEIGHT - 2 {
+        let mut can_erase = true;
+        for x in 2..FIELD_WIDTH - 2 {
+            if field[y][x] == 0 {
+                can_erase = false;
+                break;
+            }
+        }
+        if can_erase {
+            for y2 in (2..=y).rev() {
+                field[y2] = field[y2 - 1];
+            }
+        }
+    }
+}
+
+pub fn move_block(game: &mut Game, new_pos: Position) {
+    if !is_collision(&game.field, &new_pos, &game.block) {
+        game.pos = new_pos;
+    }
+}
+
+pub fn spawn_block(game: &mut Game) -> Result<(), ()> {
+    game.pos = Position::init();
+
+    game.block = BLOCKS[rand::random::<Blockkind>() as usize];
+
+    if is_collision(&game.field, &game.pos, &game.block) {
+        Err(())
+    } else {
+        Ok(())
+    }
+}
+
+pub fn gameover(game: &Game) {
+    draw(game);
+    println!("GAMEOVER");
+    println!("press 'q' key to exit");
+}
+
+pub fn quit() {
+    println!("\x1b[?25h");
+}
+
+#[allow(clippy::needless_range_loop)]
+pub fn rotate_right(game: &mut Game) {
+    let mut new_shape: BlockShape = Default::default();
+    for y in 0..4 {
+        for x in 0..4 {
+            new_shape[y][x] = game.block[4 - 1 - x][y];
+        }
+    }
+    if !is_collision(&game.field, &game.pos, &new_shape) {
+        game.block = new_shape;
+    } else if let Ok(new_pos) = super_rotation(&game.field, &game.pos, &new_shape) {
+        game.pos = new_pos;
+        game.block = new_shape;
+    }
+}
+
+#[allow(clippy::needless_range_loop)]
+pub fn rotate_left(game: &mut Game) {
+    let mut new_shape: BlockShape = Default::default();
+    for y in 0..4 {
+        for x in 0..4 {
+            new_shape[4 - 1 - x][y] = game.block[y][x];
+        }
+    }
+    if !is_collision(&game.field, &game.pos, &new_shape) {
+        game.block = new_shape;
+    } else if let Ok(new_pos) = super_rotation(&game.field, &game.pos, &new_shape) {
+        game.pos = new_pos;
+        game.block = new_shape;
+    }
+}
+
+pub fn hard_drop(game: &mut Game) {
+    while {
+        let new_pos = Position {
+            x: game.pos.x,
+            y: game.pos.y + 1,
+        };
+        !is_collision(&game.field, &new_pos, &game.block)
+    } {
+        game.pos.y += 1;
+    }
+    let new_pos = game.pos;
+    move_block(game, new_pos);
+}
+
+pub fn landing(game: &mut Game) -> Result<(), ()> {
+    fix_block(game);
+
+    erase_line(&mut game.field);
+
+    spawn_block(game)?;
+    Ok(())
+}
+
+fn ghost_pos(field: &Field, pos: &Position, block: &BlockShape) -> Position {
+    let mut ghost_pos = *pos;
+    while {
+        let new_pos = Position {
+            x: ghost_pos.x,
+            y: ghost_pos.y + 1,
+        };
+        !is_collision(field, &new_pos, block)
+    } {
+        ghost_pos.y += 1;
+    }
+    ghost_pos
+}
+
+fn super_rotation(field: &Field, pos: &Position, block: &BlockShape) -> Result<Position, ()> {
+    let diff_pos = [
+        Position {
+            x: pos.x,
+            y: pos.y.checked_sub(1).unwrap_or(pos.y),
+        },
+
+        Position {
+            x: pos.x + 1,
+            y: pos.y,
+        },
+
+        Position {
+            x: pos.x,
+            y: pos.y + 1,
+        },
+
+        Position {
+            x: pos.x.checked_sub(1).unwrap_or(pos.x),
+            y: pos.y,
+        },
+    ];
+
+    for pos in diff_pos {
+        if !is_collision(field, &pos, block) {
+            return Ok(pos);
+        }
+    }
+
+    Err(())
+}
